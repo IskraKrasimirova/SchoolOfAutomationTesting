08bf6bd5e6638ddc4344f6654218cbadc9be0837
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-11-18 20:15:42 +0200

    Extract methods for reading valid douuble numbers. Add Exponentiation and Square root operations.

--- Session1_Namespaces/SimpleCalculator/Program.cs
+++ Session1_Namespaces/SimpleCalculator/Program.cs
@@ -6,8 +6,8 @@
         {
             while (true)
             {
-                Console.WriteLine("Select one of the allowed calculator operations");
-                Console.WriteLine("Press 1 for Addition, 2 Subtraction, 3 Multiplication, 4 Division, 5 Modulus, 0 Exit");
+                Console.WriteLine("Select one of the allowed calculator operations:");
+                Console.WriteLine("Press 1 for Addition, 2 Subtraction, 3 Multiplication, 4 Division, 5 Modulus, 6 Exponentiation, 7 Square Root, 0 Exit");
 
                 string? input = Console.ReadLine();
                 bool isValidInt = Int32.TryParse(input, out int operation);
@@ -20,58 +20,121 @@
 
                 if (operation == 0)
                 {
-                    Console.WriteLine("Exit the calculator");
+                    Console.WriteLine("Exit calculator");
                     break;
                 }
 
-                if (operation < 0 || operation > 5)
+                if (operation < 0 || operation > 7)
                 {
                     Console.WriteLine("Selected operation is not supported.");
-                    continue;
-                }
-
-                Console.Write("Enter first number: ");
-                string? firstInput = Console.ReadLine();
-                bool isValidFirst = double.TryParse(firstInput, out double firstNumber);
-
-                Console.Write("Enter second number: ");
-                string? secondInput = Console.ReadLine();
-                bool isValidSecond = double.TryParse(secondInput, out double secondNumber);
-
-                if (!isValidFirst || !isValidSecond)
-                {
-                    Console.WriteLine("One or both input values are not valid numbers.");
                     continue;
                 }
 
                 switch (operation)
                 {
                     case 1:
-                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-                        break;
+                        {
+                            var (firstNumber, secondNumber) = GetTwoDoubleNumbers();
+                            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                            break;
+                        }
                     case 2:
-                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-                        break;
+                        {
+                            var (firstNumber, secondNumber) = GetTwoDoubleNumbers();
+                            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                            break;
+                        }
                     case 3:
-                        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
-                        break;
+                        {
+                            var (firstNumber, secondNumber) = GetTwoDoubleNumbers();
+                            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                            break;
+                        }
                     case 4:
-                        if (secondNumber != 0)
-                            Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-                        else
-                            Console.WriteLine("Division by zero is not allowed.");
-                        break;
+                        {
+                            var (firstNumber, secondNumber) = GetTwoDoubleNumbers();
+
+                            if (secondNumber != 0)
+                            {
+                                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Division by zero is not allowed.");
+                            }
+
+                            break;
+                        }
                     case 5:
-                        if (secondNumber != 0)
-                            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
-                        else
-                            Console.WriteLine("Modulus by zero is not allowed.");
-                        break;
+                        {
+                            var (firstNumber, secondNumber) = GetTwoDoubleNumbers();
+
+                            if (secondNumber != 0)
+                            {
+                                // Modulus with double numbers sometimes provides unexpected results and loss of precision.
+                                // For example, 120 % 0.05 = 0.04999999999999334 ~ 0
+                                Console.WriteLine($"Modulus with double: {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+                                // Casting from double to decimal does not result in a loss of precision if the values are up to 15 digits long.
+                                Console.WriteLine($"Modulus with decimal: {firstNumber} % {secondNumber} = {(decimal)firstNumber % (decimal)secondNumber}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Modulus by zero is not allowed.");
+                            }
+
+                            break;
+                        }
+                    case 6:
+                        {
+                            double baseNumber = ReadValidDoubleNumber("Enter base number: ");
+                            double exponent = ReadValidDoubleNumber("Enter exponent: ");
+                            Console.WriteLine($"{baseNumber} ^ {exponent} = {Math.Pow(baseNumber, exponent)}");
+                            break;
+                        }
+                    case 7:
+                        {
+                            double number = ReadValidDoubleNumber("Enter number to calculate square root: ");
+
+                            if (number < 0)
+                            {
+                                Console.WriteLine("Cannot calculate square root of a negative number.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Square root of {number} = {Math.Sqrt(number)}");
+                            }
+
+                            break;
+                        }
                     default:
                         Console.WriteLine("Selected operation is not supported.");
                         break;
                 }
             }
+        }
+
+        private static double ReadValidDoubleNumber(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string? input = Console.ReadLine();
+                bool isValidDoubleNumber = double.TryParse(input, out double number);
+
+                if (isValidDoubleNumber)
+                {
+                    return number;
+                }
+
+                Console.WriteLine("Input is not a valid number. Please try again.");
+            }
+        }
+
+        private static (double firstNumber, double secondNumber) GetTwoDoubleNumbers()
+        {
+            double firstNumber = ReadValidDoubleNumber("Enter first number: ");
+            double secondNumber = ReadValidDoubleNumber("Enter second number: ");
+            return (firstNumber, secondNumber);
         }
     }
 }
