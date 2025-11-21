1810cb12a77b703f61c17b2a3c142ead8f78646b
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-11-21 16:29:45 +0200

    Add Age problem

---
+++ Session2_IfStatements/MinorOrAdult/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace MinorOrAdult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            while (true)
+            {
+                Console.Write("Enter your age: ");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out age))
+                {
+                    if (age >= 0 && age <= 120)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid age. Please enter an integer value between 0 and 120.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid age as integer number.");
+                }
+            }
+
+            //if (age < 18)
+            //{
+            //    Console.WriteLine("You are a minor.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are an adult.");
+            //}
+
+            string result = age < 18 ? "You are a minor." : "You are an adult.";
+            Console.WriteLine(result);
+        }
+    }
+}
