87552e5d5f54e8da4457f3ff9560879c4b02515a
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-11-25 14:28:41 +0200

    Refacoring IsPrime

--- Session3_Loops/PrimaryNumbers/Program.cs
+++ Session3_Loops/PrimaryNumbers/Program.cs
@@ -42,31 +42,46 @@ namespace PrimaryNumbers
 
         private static bool IsPrime(int number)
         {
-            bool isPrime = true;
+            if (number == 2) return true;
+            if ((number % 2) == 0) return false;
 
-            if (number == 2)
-            {
-                isPrime = true;
-            }
-            else if ((number % 2) == 0)
-            {
-                isPrime = false;
-            }
-            else
-            {
-                int boundary = (int)Math.Sqrt(number);
+            int boundary = (int)Math.Sqrt(number);
 
-                for (int i = 3; i <= boundary; i += 2)
-                {
-                    if ((number % i) == 0)
-                    {
-                        isPrime = false;
-                        break;
-                    }
-                }
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if ((number % i) == 0) return false;
             }
 
-            return isPrime;
+            return true;
         }
+
+        //private static bool IsPrime(int number)
+        //{
+        //    bool isPrime = true;
+
+        //    if (number == 2)
+        //    {
+        //        isPrime = true;
+        //    }
+        //    else if ((number % 2) == 0)
+        //    {
+        //        isPrime = false;
+        //    }
+        //    else
+        //    {
+        //        int boundary = (int)Math.Sqrt(number);
+
+        //        for (int i = 3; i <= boundary; i += 2)
+        //        {
+        //            if ((number % i) == 0)
+        //            {
+        //                isPrime = false;
+        //                break;
+        //            }
+        //        }
+        //    }
+
+        //    return isPrime;
+        //}
     }
 }
