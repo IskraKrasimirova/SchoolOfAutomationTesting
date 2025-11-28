6af1f914f7b6e240f592b1cd7bef3087db769efb
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-11-28 13:44:42 +0200

    Add Greeting and Find the biggest element in array

---
+++ Session4_Methods/Greeting/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter your name (Type End for exit): ");
+                var name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("The name cannot empty or whitespace.");
+                    continue;
+                }
+
+                if (name.Equals("end", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("End of program.");
+                    break;
+                }
+
+                var greeting = MagicGreeting(name);
+                Console.WriteLine(greeting);
+            }
+           
+        }
+
+        private static string MagicGreeting(string name)
+        {
+            string[] greetings =
+            [
+                $"Welcome, mighty {name}!",
+                $"Greetings, traveler {name}!",
+                $"Behold, the legendary {name} has arrived!",
+                $"A new star {name} appeared!",
+                $"{name} joined the party!",
+                $"{name} appeared on the scene!",
+                $"Cheers for {name}!"
+            ];
+
+            var random = new Random();
+            int index = random.Next(greetings.Length);
+
+            return greetings[index];
+        }
+    }
+}
+
---
+++ Session4_Methods/TheBiggestElement/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace BiggestElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -3, 5, 7, -2, 8, int.MinValue, 1, 4, 0, -123456789 };
+
+            // Using LINQ
+            //int biggestElement = array.Max();
+            //Console.WriteLine($"The biggest element in the array is {biggestElement}.");
+
+            //int[] sortedArray = array.OrderByDescending(x => x).ToArray();
+
+            //Console.WriteLine($"Sorted descending array: {string.Join(", ", sortedArray)}");
+
+            // ****************************************************************
+            // Using method that returns index of the biggest element with start index
+            //var maxIndex = FindMaxElementIndex(array, 0);
+            //Console.WriteLine($"The biggest element in the array is {array[maxIndex]} at index {maxIndex}.");
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    var maxIdx = FindMaxElementIndex(array, i);
+
+            //    //int temp = array[i];
+            //    //array[i] = array[maxIdx];
+            //    //array[maxIdx] = temp;
+
+            //    // tuple swap from C# 7.0
+            //    (array[maxIdx], array[i]) = (array[i], array[maxIdx]);
+            //}
+
+            //Console.WriteLine($"Sorted descending array: {string.Join(", ", array)}");
+
+            // ****************************************************************
+            // Using method that returns index of the biggest element
+            var maxIndex = FindMaxElementIndex(array);
+            Console.WriteLine($"The biggest element in the array is {array[maxIndex]} at index {maxIndex}.");
+
+            int[] sortedDescArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int maxIdx = FindMaxElementIndex(array);
+                sortedDescArray[i] = array[maxIdx];
+                array[maxIdx] = int.MinValue;
+            }
+
+            Console.WriteLine($"Sorted descending array: {string.Join(", ", sortedDescArray)}");
+        }
+
+        private static int FindMaxElementIndex(int[] array)
+        {
+            int maxIndex = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        private static int FindMaxElementIndex(int[] array, int startIndex)
+        {
+            int maxIndex = startIndex;
+
+            for (int i = startIndex + 1; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        private static (int index, int maxValue) FindMaxElementAndIndex(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                Console.WriteLine("Array cannot be null or empty.");
+            }
+
+            int maxIndex = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return (maxIndex, array[maxIndex]);
+        }
+    }
+}
+
+//6.Write a method that finds the biggest element of an array. Use that method to implement sorting in descending order.
