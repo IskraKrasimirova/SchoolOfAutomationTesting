6ffa17536c94022202cf12fb3a4693fea987a099
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-12-05 18:07:47 +0200

    Add method that groups songs by first letter and GetCustomPlaylistByDesiredLenght method using simple greedy algorithm, not optimal decision

--- Session7_Collection/MusicShuffleMachine/Program.cs
+++ Session7_Collection/MusicShuffleMachine/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace MusicShuffleMachine
+﻿using System.ComponentModel;
+
+namespace MusicShuffleMachine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<(string song, string duration)> songs = new()
-            {
+            List<(string, string)> songs =
+            [
                 ("Nothing Else Matters", "6:28"),               // Metallica
                 ("November Rain", "8:57"),                      // Guns N' Roses
                 ("Stairway to Heaven", "8:02"),                 // Led Zeppelin
@@ -28,23 +30,88 @@
                 ("Creep", "3:58"),                              // Radiohead
                 ("Every Breath You Take", "4:13"),              // The Police
                 ("I Want to Know What Love Is", "5:04"),        // Foreigner
-                ("(Everything I Do) I Do It for You", "6:38"),  // Bryan Adams
+                ("Everything I Do", "6:38"),                    // Bryan Adams
                 ("Open Your Heart", "4:29"),                    // Madonna
                 ("One", "4:36")                                 // U2
-            };
+            ];
 
+            Console.Write("Enter desired playlist length in minutes: ");
+            var isValidLength = int.TryParse(Console.ReadLine(), out int desiredLength);
+
+            if (isValidLength && desiredLength > 0)
+            {
+                var customPlaylist = GetCustomPlaylistByDesiredLenght(songs, desiredLength);
+                Console.WriteLine($"Custom playlist for {desiredLength} minutes and {customPlaylist.Count} songs:");
+
+                foreach (var (song, durationInSeconds) in customPlaylist)
+                {
+                    Console.WriteLine($"{song} - {durationInSeconds / 60}:{durationInSeconds % 60:D2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for playlist length.");
+            }
+
+            var groupedSongs = GroupSongsByFirstLetter(songs);
+            var sortedGroupedSongs = groupedSongs.OrderBy(k => k.Key).ToDictionary();
+            Console.WriteLine("Songs grouped by first letter:");
+
+            foreach (var kvp in sortedGroupedSongs)
+            {
+                var quatedSong = kvp.Value.Select(s => $"\"{s}\"");
+                Console.WriteLine($"{kvp.Key}: {string.Join(", ", quatedSong)}");
+            }
+        }
+
+        private static Dictionary<char, List<string>> GroupSongsByFirstLetter(List<(string, string)> songs)
+        {
+            var groupedSongs = new Dictionary<char, List<string>>();
+
+            foreach (var (song, duration) in songs)
+            {
+                var firstLetter = char.ToUpper(song[0]);
+
+                if (!groupedSongs.ContainsKey(firstLetter))
+                {
+                    groupedSongs[firstLetter] = new List<string>();
+                }
+
+                groupedSongs[firstLetter].Add(song);
+            }
+
+            return groupedSongs;
+        }
+
+        private static List<(string, int)> GetCustomPlaylistByDesiredLenght(List<(string, string)> songs, int length)
+        {
+            var legthInSeconds = length * 60;
+            var totalDuration = 0;
+            var customPlaylist = new List<(string, int)>();
             var shuffledSongs = ShuffleSongs(songs);
 
             foreach (var (song, duration) in shuffledSongs)
             {
                 var durationInSeconds = ConvertSongDurationToSeconds(duration);
-                Console.WriteLine($"{song} - {duration} -> {durationInSeconds}");
+
+                if (totalDuration + durationInSeconds <= legthInSeconds)
+                {
+                    customPlaylist.Add((song, durationInSeconds));
+                    totalDuration += durationInSeconds;
+                }
+
+                if (totalDuration >= legthInSeconds)
+                {
+                    break;
+                }
             }
+
+            return customPlaylist;
         }
 
-        private static List<(string song, string duration)> ShuffleSongs(List<(string song, string duration)> songs)
+        private static List<(string song, string duration)> ShuffleSongs(List<(string, string)> songs)
         {
-            var shuffledSongs = new List<(string song, string duration)>(songs);
+            var shuffledSongs = new List<(string, string)>(songs);
             var random = new Random();
 
             for (int i = shuffledSongs.Count - 1; i > 0; i--)
